4857be6c139432e5e5ee57f92cfd397222790bc0
LuisFaria <57047301+luisfcfaria@users.noreply.github.com>
2022-06-01 15:23:04 +0100

    starter setup

---
+++ src/main/java/mindera/solverde/logstarter/LogLibraryAutoConfiguration.java
@@ -0,0 +1,25 @@
+package mindera.solverde.logstarter;
+
+import mindera.solverde.logfilter.RequestResponseLoggingFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass(RequestResponseLoggingFilter.class)
+public class LogLibraryAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public FilterRegistrationBean<RequestResponseLoggingFilter> loggingFilter(){
+        FilterRegistrationBean<RequestResponseLoggingFilter> registrationBean = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new RequestResponseLoggingFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(2);
+
+        return registrationBean;
+    }
+}
