17c11d85fb194ee9e2fbcb35d140ed49fab324e7
LuisFaria <57047301+luisfcfaria@users.noreply.github.com>
2022-06-09 14:58:58 +0100

    updated package

---
+++ src/main/java/mindera/logstarter/LogLibraryAutoConfiguration.java
@@ -0,0 +1,26 @@
+package mindera.logstarter;
+
+
+import mindera.logfilter.service.RequestResponseLoggingFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass(RequestResponseLoggingFilter.class)gis
+public class LogLibraryAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public FilterRegistrationBean<RequestResponseLoggingFilter> loggingFilter(){
+        FilterRegistrationBean<RequestResponseLoggingFilter> registrationBean = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new RequestResponseLoggingFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(2);
+
+        return registrationBean;
+    }
+}
