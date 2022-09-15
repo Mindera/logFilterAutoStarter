e8b5f17e8c34ef233720501c35578e6b2f6542f9
alicjakropa <alakrop@gmail.com>
2022-09-15 13:32:54 +0100

    pom update: preparing the new version release

--- src/main/java/mindera/logstarter/LogLibraryAutoConfiguration.java
+++ src/main/java/mindera/logstarter/LogLibraryAutoConfiguration.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConditionalOnClass(RequestResponseLoggingFilter.class)
 public class LogLibraryAutoConfiguration {
-
     @Bean
     @ConditionalOnMissingBean
     public FilterRegistrationBean<RequestResponseLoggingFilter> loggingFilter(){
